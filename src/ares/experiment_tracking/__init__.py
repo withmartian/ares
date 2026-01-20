4d4faa40b7921800d5cb1d6f630b7d668638cb6c
Nithin <sarvanithin@gmail.com>
2026-01-20 01:28:48 -0500

    Keep __init__.py empty per repo design decision

--- src/ares/experiment_tracking/__init__.py
+++ src/ares/experiment_tracking/__init__.py
@@ -1,19 +1,1 @@
 """Experiment tracking and statistics."""
-
-from ares.experiment_tracking.stat_tracker import LoggingStatTracker
-from ares.experiment_tracking.stat_tracker import NullStatTracker
-from ares.experiment_tracking.stat_tracker import StatTracker
-
-__all__ = [
-    "LoggingStatTracker",
-    "NullStatTracker",
-    "StatTracker",
-]
-
-# TensorboardStatTracker is optionally available if torch is installed
-try:
-    from ares.experiment_tracking.tensorboard import TensorboardStatTracker  # noqa: F401
-
-    __all__.append("TensorboardStatTracker")
-except ImportError:
-    pass
